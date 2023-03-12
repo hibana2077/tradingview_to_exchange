d7f12d324f1b3b0f1f8603e2dff4ef148593628a
hibana2077 <hibana2077@gmail.com>
2023-03-12 10:42:45 +0800

    2023-03-12

--- doc/welcome.md
+++ doc/welcome.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2023-03-12 10:40:05
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-03-12 10:42:37
+ * @FilePath: \tradingview_to_exchange\doc\welcome.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # Welcome
 
 If you see this page in your browser, you have successfully installed the
---
+++ src/user.py
@@ -0,0 +1,41 @@
+import requests
+import streamlit as st
+
+
+
+def welcome():
+    st.title("Welcome")
+    st.markdown()
+
+def login():
+    st.title("Login")
+    host_location = st.text_input("Host Location(IP Address)")
+    user_name = st.text_input("User Name")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        st.session_state.host_location = host_location
+        st.session_state.user_name = user_name
+        st.session_state.password = password
+        response = requests.post(f"http://{host_location}:80/login", json={"user_name": user_name, "password": password})
+        if response.status_code == 200:
+            if response.json()["status"] == "Success":
+                st.session_state.token = response.json()["token"]
+                st.session_state.user_id = response.json()["user_id"]
+                st.session_state.user_name = response.json()["user_name"]
+            elif response.json()["status"] == "Failure":
+                st.error(response.json()["message"])
+        else:
+            st.error("Login Failed - Please check your host location and credentials")
+        
+
+
+pages = {
+    "Welcome": welcome,
+    "Login": login,
+}
+
+if __name__ == "__main__":
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.selectbox("Go to", list(pages.keys())) if st.session_state.get("host_location") is not None else "Login"
+    page = pages[selection]
+    page()
