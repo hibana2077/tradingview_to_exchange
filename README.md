a446027118adc6abcc76fadc45adfdc875eb16f0
hibana2077 <hibana2077@gmail.com>
2023-03-12 11:51:19 +0800

    2023-03-12

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ English | [中文](./doc/README_TW.md)
 ![fastapi](https://img.shields.io/badge/fastapi-0.85.1-009688?style=plastic-square&logo=fastapi)
 ![mongodb](https://img.shields.io/badge/mongodb-4.4.6-47A248?style=plastic-square&logo=mongodb)
 ![openai](https://img.shields.io/badge/openai-0.27.0-412991?style=plastic-square&logo=openai)
+![docker](https://img.shields.io/badge/docker-20.10.11-2496ED?style=plastic-square&logo=docker)
+[![Binance](https://img.shields.io/badge/binance--%2d10%25-F0B90B?style=plastic-square&logo=binance)](https://www.binance.com/en/activity/referral-entry/MYB23J?fromActivityPage=true&ref=LIMIT_TJMU1KAZ)
+[![Okex](https://img.shields.io/badge/okex--%2d10%25-000000?style=plastic-square&logo=okex)](https://www.okx.com/join/18323483)
 
 This is a Crypto trading bot that can automatically trade on the exchange based on the tradingview alert.
 
@@ -26,3 +29,38 @@ User only need to deploy docker image on the server and set the tradingview aler
 - [x] Support Binance, Okex
 - [x] Support multiple trading pairs
 - [x] Support multiple trading strategies
+
+## Installation
+
+### 1. Install Docker
+
+- [Install Docker](https://docs.docker.com/engine/install/)
+- [Install Docker Compose](https://docs.docker.com/compose/install/)
+
+### 2. Clone the repository
+
+```bash
+git clone https://github.com/hibana2077/tradingview_to_exchange
+```
+
+### 3. Run the docker-compose
+
+```bash
+cd tradingview_to_exchange
+docker-compose up -d
+```
+
+You can set account information in the `docker-compose.yml` file.
+
+### 4. Setup firewall
+
+- Open port 80 and 27017 on the server
+
+### 5. Login to the web
+
+- Open the browser and clikc the [link](https://hibana2077-gpt-news-generator-srcmain-0osgu0.streamlit.app/)
+- Login with the default account and password and host address(if you don't change the account information in the `docker-compose.yml` file)
+    - Account: admin
+    - Password: admin
+    - Host: IP address of the server
+
--- src/api.py
+++ src/api.py
@@ -2,14 +2,14 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2023-03-11 23:14:36
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-03-12 02:18:12
+LastEditTime: 2023-03-12 11:40:47
 FilePath: \tradingview_to_exchange\src\api.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
-from ccxt import binance
+from ccxt import binance,okex5
 import pymongo
 import uvicorn
 import argparse
@@ -19,8 +19,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--mongo', type=str, default='mongodb://localhost:27017/', help='MongoDB connection string')
 parser.add_argument('--user_name', type=str, default='admin', help='APP user name')
 parser.add_argument('--password', type=str, default='admin', help='APP password')
+parser.add_argument('--broker', type=str, default='080c0d187dcaSUDE', help='Broker ID')
 
 args = parser.parse_args()
+
+def record_failed_order(order):
+    myclient = pymongo.MongoClient(args.mongo)
+    mydb = myclient["tradingview_to_exchange"]
+    mycol = mydb["failed_orders"]
+    mycol.insert_one(order)
 
 class Order(BaseModel):
     api_key: str # API key for the exchange
@@ -45,69 +52,7 @@ def read_root():
 
 @app.post("/order")
 def create_order(order: Order):
-    if order.class_SF == "Spot":
-        binance_spot = binance({
-            'apiKey': order.api_key,
-            'enableRateLimit': True,
-            'options': {
-                'defaultType': 'spot',
-            },
-        })
-        if order.type == "Market":
-            order = binance_spot.create_order(
-                symbol=order.symbol,
-                type="market",
-                side=order.side,
-                amount=order.quantity
-            )
-        elif order.type == "Limit":
-            order = binance_spot.create_order(
-                symbol=order.symbol,
-                type="limit",
-                side=order.side,
-                amount=order.quantity,
-                price=order.price
-            )
-        else:
-            my_client = pymongo.MongoClient(args.mongo)
-            my_db = my_client["tradingview_to_exchange"]
-            my_col = my_db["orders"]
-            my_dict = {
-                "api_key": order.api_key,
-                "symbol": order.symbol,
-                "side": order.side,
-                "type": order.type,
-                "quantity": order.quantity,
-                "price": order.price,
-                "class_SF": order.class_SF,
-                "webhook": order.webhook,
-                "status": "Error: Invalid order type"
-            }
-            my_col.insert_one(my_dict)
-            return {"status": "Error: Invalid order type"}
-    elif order.class_SF == "Future":
-        binance_future = binance({
-            'apiKey': order.api_key,
-            'enableRateLimit': True,
-            'options': {
-                'defaultType': 'future',
-            },
-        })
-        if order.type == "Market":
-            order = binance_future.create_order(
-                symbol=order.symbol,
-                type="market",
-                side=order.side,
-                amount=order.quantity
-            )
-        elif order.type == "Limit":
-            order = binance_future.create_order(
-                symbol=order.symbol,
-                type="limit",
-                side=order.side,
-                amount=order.quantity,
-                price=order.price
-            )
+    
 
 while True:
     try:
