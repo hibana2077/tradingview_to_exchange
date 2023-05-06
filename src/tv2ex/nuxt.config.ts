7e9df24b8686333e0ad4df81da9ef9258bc98428
hibana2077 <hibana2077@gmail.com>
2023-05-06 14:37:50 +0800

    tring test primevue

--- src/tv2ex/nuxt.config.ts
+++ src/tv2ex/nuxt.config.ts
@@ -1,4 +1,19 @@
+/*
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2023-05-06 13:59:42
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-05-06 14:33:03
+ * @FilePath: \tradingview_to_exchange\src\tv2ex\nuxt.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    css: [
+        "primevue/resources/themes/lara-light-blue/theme.css",
+        "primevue/resources/primevue.css",
+        "primeicons/primeicons.css",
+    ],
+	build: {
+		transpile: ["primevue"]
+	}
 })
---
+++ src/tv2ex/plugins/primevue.js
@@ -0,0 +1,15 @@
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+//ToastService is a virable name that you can change it to any name you want
+
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ToastService)
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Toast', Toast)
+    //other components that you need
+})
